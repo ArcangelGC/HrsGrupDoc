34ef2c7669a16f0acf876cd3c271cf848d6e39fe
JAntonioCoder <158400772+JAntonioCoder@users.noreply.github.com>
2024-05-23 12:11:48 -0600

    Prueba commit

--- APP_REGISTRO_DE_HORARIOS/PAG_PRIN.Designer.cs
+++ APP_REGISTRO_DE_HORARIOS/PAG_PRIN.Designer.cs
@@ -58,19 +58,18 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1304, 101);
+            this.panel1.Size = new System.Drawing.Size(978, 82);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(66, 4);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(50, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(113, 90);
+            this.pictureBox1.Size = new System.Drawing.Size(85, 73);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -80,11 +79,12 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(216, 23);
+            this.label1.Location = new System.Drawing.Point(162, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(924, 56);
+            this.label1.Size = new System.Drawing.Size(774, 45);
             this.label1.TabIndex = 0;
-            this.label1.Text = "INSTITUTO TECNOLOGICO DE TLAXIACO";
+            this.label1.Text = "\"INSTITUTO TECNOLOGICO DE TLAXIACO\"";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel2
@@ -93,20 +93,20 @@
             this.panel2.Controls.Add(this.panelformularios);
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 101);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(0, 82);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1304, 523);
+            this.panel2.Size = new System.Drawing.Size(978, 425);
             this.panel2.TabIndex = 1;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // panelformularios
             // 
             this.panelformularios.Controls.Add(this.rjButton1);
-            this.panelformularios.Location = new System.Drawing.Point(198, 0);
-            this.panelformularios.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelformularios.Location = new System.Drawing.Point(148, 0);
+            this.panelformularios.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelformularios.Name = "panelformularios";
-            this.panelformularios.Size = new System.Drawing.Size(1106, 523);
+            this.panelformularios.Size = new System.Drawing.Size(830, 425);
             this.panelformularios.TabIndex = 8;
             this.panelformularios.Paint += new System.Windows.Forms.PaintEventHandler(this.panelformularios_Paint);
             // 
@@ -122,10 +122,10 @@
             this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rjButton1.ForeColor = System.Drawing.Color.Black;
-            this.rjButton1.Location = new System.Drawing.Point(169, 78);
-            this.rjButton1.Margin = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.rjButton1.Location = new System.Drawing.Point(127, 63);
+            this.rjButton1.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.rjButton1.Name = "rjButton1";
-            this.rjButton1.Size = new System.Drawing.Size(175, 49);
+            this.rjButton1.Size = new System.Drawing.Size(131, 40);
             this.rjButton1.TabIndex = 1;
             this.rjButton1.TextColor = System.Drawing.Color.Black;
             this.rjButton1.UseVisualStyleBackColor = false;
@@ -143,9 +143,9 @@
             this.panel3.Controls.Add(this.rjButton3);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(195, 523);
+            this.panel3.Size = new System.Drawing.Size(146, 425);
             this.panel3.TabIndex = 7;
             // 
             // bttn_BUSCAR
@@ -155,10 +155,10 @@
             this.bttn_BUSCAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttn_BUSCAR.Font = new System.Drawing.Font("Arial Black", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bttn_BUSCAR.ForeColor = System.Drawing.Color.Black;
-            this.bttn_BUSCAR.Location = new System.Drawing.Point(15, 271);
-            this.bttn_BUSCAR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bttn_BUSCAR.Location = new System.Drawing.Point(11, 220);
+            this.bttn_BUSCAR.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bttn_BUSCAR.Name = "bttn_BUSCAR";
-            this.bttn_BUSCAR.Size = new System.Drawing.Size(164, 74);
+            this.bttn_BUSCAR.Size = new System.Drawing.Size(123, 60);
             this.bttn_BUSCAR.TabIndex = 6;
             this.bttn_BUSCAR.Text = "BUSCAR HORARIO DE DOCENTE";
             this.bttn_BUSCAR.UseVisualStyleBackColor = false;
@@ -171,10 +171,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial Black", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(3, 385);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(2, 313);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 49);
+            this.button1.Size = new System.Drawing.Size(135, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "BUSCAR HORARIO DE GRUPO";
             this.button1.UseVisualStyleBackColor = false;
@@ -191,10 +191,9 @@
             this.rjButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rjButton4.ForeColor = System.Drawing.Color.Black;
-            this.rjButton4.Location = new System.Drawing.Point(0, 263);
-            this.rjButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.rjButton4.Location = new System.Drawing.Point(0, 214);
             this.rjButton4.Name = "rjButton4";
-            this.rjButton4.Size = new System.Drawing.Size(191, 92);
+            this.rjButton4.Size = new System.Drawing.Size(143, 75);
             this.rjButton4.TabIndex = 8;
             this.rjButton4.TextColor = System.Drawing.Color.Black;
             this.rjButton4.UseVisualStyleBackColor = false;
@@ -210,10 +209,9 @@
             this.btn_Crear_H1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Crear_H1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Crear_H1.ForeColor = System.Drawing.Color.Black;
-            this.btn_Crear_H1.Location = new System.Drawing.Point(0, 366);
-            this.btn_Crear_H1.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Crear_H1.Location = new System.Drawing.Point(0, 297);
             this.btn_Crear_H1.Name = "btn_Crear_H1";
-            this.btn_Crear_H1.Size = new System.Drawing.Size(191, 92);
+            this.btn_Crear_H1.Size = new System.Drawing.Size(143, 75);
             this.btn_Crear_H1.TabIndex = 0;
             this.btn_Crear_H1.TextColor = System.Drawing.Color.Black;
             this.btn_Crear_H1.UseVisualStyleBackColor = false;
@@ -226,10 +224,10 @@
             this.btn_C_H_G.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_C_H_G.Font = new System.Drawing.Font("Arial Black", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_C_H_G.ForeColor = System.Drawing.Color.Black;
-            this.btn_C_H_G.Location = new System.Drawing.Point(15, 172);
-            this.btn_C_H_G.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_C_H_G.Location = new System.Drawing.Point(11, 140);
+            this.btn_C_H_G.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_C_H_G.Name = "btn_C_H_G";
-            this.btn_C_H_G.Size = new System.Drawing.Size(164, 65);
+            this.btn_C_H_G.Size = new System.Drawing.Size(123, 53);
             this.btn_C_H_G.TabIndex = 7;
             this.btn_C_H_G.Text = "CREAR HORARIO PARA GRUPO";
             this.btn_C_H_G.UseVisualStyleBackColor = false;
@@ -244,10 +242,10 @@
             this.btn_Crear_H.Font = new System.Drawing.Font("Arial Black", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Crear_H.ForeColor = System.Drawing.Color.Black;
             this.btn_Crear_H.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_Crear_H.Location = new System.Drawing.Point(15, 66);
-            this.btn_Crear_H.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Crear_H.Location = new System.Drawing.Point(11, 54);
+            this.btn_Crear_H.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Crear_H.Name = "btn_Crear_H";
-            this.btn_Crear_H.Size = new System.Drawing.Size(168, 60);
+            this.btn_Crear_H.Size = new System.Drawing.Size(126, 49);
             this.btn_Crear_H.TabIndex = 3;
             this.btn_Crear_H.Text = "CREAR HORARIO PARA DOCENTE";
             this.btn_Crear_H.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -265,10 +263,9 @@
             this.rjButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rjButton2.ForeColor = System.Drawing.Color.Black;
-            this.rjButton2.Location = new System.Drawing.Point(0, 52);
-            this.rjButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.rjButton2.Location = new System.Drawing.Point(0, 42);
             this.rjButton2.Name = "rjButton2";
-            this.rjButton2.Size = new System.Drawing.Size(191, 92);
+            this.rjButton2.Size = new System.Drawing.Size(143, 75);
             this.rjButton2.TabIndex = 1;
             this.rjButton2.TextColor = System.Drawing.Color.Black;
             this.rjButton2.UseVisualStyleBackColor = false;
@@ -285,22 +282,21 @@
             this.rjButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rjButton3.ForeColor = System.Drawing.Color.Black;
-            this.rjButton3.Location = new System.Drawing.Point(0, 160);
-            this.rjButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.rjButton3.Location = new System.Drawing.Point(0, 130);
             this.rjButton3.Name = "rjButton3";
-            this.rjButton3.Size = new System.Drawing.Size(191, 92);
+            this.rjButton3.Size = new System.Drawing.Size(143, 75);
             this.rjButton3.TabIndex = 4;
             this.rjButton3.TextColor = System.Drawing.Color.Black;
             this.rjButton3.UseVisualStyleBackColor = false;
             // 
             // PAG_PRIN
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1304, 624);
+            this.ClientSize = new System.Drawing.Size(978, 507);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PAG_PRIN";
             this.Text = "Pagina Principal";
             this.Load += new System.EventHandler(this.PAG_PRIN_Load);
